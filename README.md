93d53d6d2010fc309e72b89b7992f0dd6c056243
intika <intika@librefox.org>
2020-05-02 00:18:08 +0200

    v3.5 adding isplaying, fix python version and update spotty command

--- README.md
+++ README.md
@@ -7,29 +7,27 @@ Control spotifyd/spotify with dbus and web-api...
 
 ![Alt text](screens-2.png?raw=true "Advanced")
 
-**Features not listed on the captures:**
-
-- isplaying
-
 # Install:   
 ```
-#   
-# Install:   
-# 1. change the python value on the first line to match your version (using python3.6 by default)... 
-#    this application require python3.x
-# 2. Check the required applications/packages bellow
-# 3. Run 'chmod 755 ./spotimaster.py'
-# 4. Link or copy the application to bin folder with
+# 
+# 1. Check the required applications/packages bellow
+# 2. Run 'chmod 755 ./spotimaster.py'
+# 3. Link or copy the application to bin folder with
 #    'ln -s ./spotimaster.py /usr/bin/spotimaster'
-# 5. Install the required packages...  
+# 4. Install the required packages...  
 # required packages are: dbus-python, requests, urllib3 ('pip3 install dbus-python requests urllib3')
 # required applications: spotifyd/spotify, spotty for web-api functions and dbus-send for openuri 
 # command spotty is available here: https://github.com/michaelherger/spotty 
 # git clone it then use 'cargo build --release'.
 #
-# You do need an app-id to use spotty, you can generate it here 
+# To use this app you do need an app-id; you can generate it here 
 # https://developer.spotify.com/dashboard/applications/
-# app-secret and web authorisations, are not required.
+# app-secret, web authorizations, login and password are not needed.
+# you only need the app-id :), note that the app-id need to be yours (belong to the target account) 
+# for this to work. 
+#
+# Disclaimer : using spotty's code to connect to Spotify's API is probably forbidden by them. 
+# Use at your own risk.  
 #
 # Note :
 # - Change the cache location if required by editing the variable 'cache_location' bellow
--- spotimaster.py
+++ spotimaster.py
@@ -8,24 +8,32 @@ Link = 'https://github.com/Intika-Linux-Spotify/Spotimaster'
 # intika - http://github.com/intika
 
 #   
-# Install:   
-# 1. Change the python value on the first line to match your version (using python3.6 by default)... 
-#    this application require python3.x
-# 2. Check the required applications/packages bellow
-# 3. Run 'chmod 755 ./spotimaster.py'
-# 4. Link or copy the application to bin folder with
+# Install:
+# 1. Check the required applications/packages bellow
+# 2. Run 'chmod 755 ./spotimaster.py'
+# 3. Link or copy the application to bin folder with
 #    'ln -s ./spotimaster.py /usr/bin/spotimaster'
-# 5. Install the required packages...  
+# 4. Install the required packages...  
 # required packages are: dbus-python, requests, urllib3 ('pip3 install dbus-python requests urllib3')
-# required applications: spotifyd/spotify, spotty for web-api functions and dbus-send for openuri command
-# spotty is available here: https://github.com/michaelherger/spotty, git clone it then use 'cargo build --release'
-# you do need an app-id to use spotty, you can generate it here https://developer.spotify.com/dashboard/applications/
-# app-secret and web authorisations (redirects...) are not required.
+# required applications: spotifyd/spotify, spotty for web-api functions and dbus-send for openuri 
+# command spotty is available here: https://github.com/michaelherger/spotty 
+# git clone it then use 'cargo build --release'.
+#
+# To use this app you do need an app-id; you can generate it here 
+# https://developer.spotify.com/dashboard/applications/
+# app-secret, web authorizations, login and password are not needed.
+# you only need the app-id :), note that the app-id need to be yours (belong to the target account) 
+# for this to work. 
+#
+# Disclaimer : using spotty's code to connect to Spotify's API is probably forbidden by them. 
+# Use at your own risk.  
 #
 # Note :
 # - Change the cache location if required by editing the variable 'cache_location' bellow
-# - If you want to save spotty parameters directly here, you can edit the variable 'spotty_params' bellow, leave empty if not used. 
-# - If you want to use this application's dbus function with an other player change the playername bellow
+# - If you want to save spotty parameters directly here, you can edit the variable 
+#   'spotty_params' bellow, leave empty if not used. 
+# - If you want to use this application's dbus function with an other player change 
+#   the playername bellow
 #
 
 cache_location = '/tmp'
@@ -134,7 +142,7 @@ if (not len(sys.argv) > 1) or (sys.argv[1] == '--help'):
     print('')
     print('                 examples:')
     print('\033[0;32;40m')
-    print('                 spotimaster -love -n APP-NAME-WHATEVER -c /tmp/spotty -u USERNAME -t -i MY-APP-ID -p MYPASSWORD \\') 
+    print('                 spotimaster -love -n MY-APP-NAME -c /tmp/spotty -t -i MY-APP-ID \\') 
     print('                 --scope user-modify-playback-state,user-library-modify,user-read-currently-playing,user-read-playback-state')
     print('\033[0;37;40m')
     print('')
